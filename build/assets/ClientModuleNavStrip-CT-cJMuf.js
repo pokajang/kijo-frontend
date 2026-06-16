646a54c401f6da7e2d49e27e06ddca768c548080
Azam Husain <azamhusain9@gmail.com>
2026-06-16 12:55:00 +0800

    Refresh frontend production build

---
+++ build/assets/ClientModuleNavStrip-CT-cJMuf.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-react-CQ7HMlZ1.js";import{M as r}from"./ModuleNavStrip-BX64wZit.js";import{a1 as a}from"./DefaultLayout-mg3KEvZW.js";const l=t=>o.jsx(r,{ariaLabel:"Client sections",...t,tabs:a});export{l as C};
---
+++ build/assets/recordsTableUiShared-CkZt-GQi.js
@@ -0,0 +1,1 @@
+import{a4 as e}from"./DefaultLayout-mg3KEvZW.js";const r="lg",t=e;export{t as c,r};
