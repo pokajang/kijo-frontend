f50e69491ee10a0789b6138d36fe8fd64d29ef37
Azam Husain <azamhusain9@gmail.com>
2026-06-22 19:18:52 +0800

    Build frontend release assets

---
+++ build/assets/ClientModuleNavStrip-CA4KccGI.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-react-DN-RL_pw.js";import{M as r}from"./ModuleNavStrip-D8ZK3N_1.js";import{a3 as a}from"./DefaultLayout-DA2-kl6M.js";const l=t=>o.jsx(r,{ariaLabel:"Client sections",...t,tabs:a});export{l as C};
