fc0b4ffa3c0e9b1e96f377a670bcd3fde88b9789
Azam Husain <azamhusain9@gmail.com>
2026-07-17 09:51:48 +0800

    chore(frontend): refresh production build

---
+++ build/assets/ClientModuleNavStrip-CHqyz0yL.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-react-DN-RL_pw.js";import{M as r}from"./ModuleNavStrip-ChHMD7Sm.js";import{a3 as a}from"./DefaultLayout-DWSHo2Jt.js";const l=t=>o.jsx(r,{ariaLabel:"Client sections",...t,tabs:a});export{l as C};
---
+++ build/assets/recordsTableUiShared-Djj5pIY7.js
@@ -0,0 +1,1 @@
+import{a6 as e}from"./DefaultLayout-DWSHo2Jt.js";const r="lg",t=e;export{t as c,r};
