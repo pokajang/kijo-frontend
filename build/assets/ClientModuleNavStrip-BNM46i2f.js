bfa4f284cb750c6a9a39bc3d091eba63897bb9db
Azam Husain <azamhusain9@gmail.com>
2026-07-27 16:50:15 +0800

    build: publish IH and handbook frontend assets

---
+++ build/assets/ClientModuleNavStrip-BNM46i2f.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-react-DN-RL_pw.js";import{M as r}from"./ModuleNavStrip-YhtyDMmm.js";import{a3 as a}from"./DefaultLayout-CKhTvIqn.js";const l=t=>o.jsx(r,{ariaLabel:"Client sections",...t,tabs:a});export{l as C};
