ea6a7c0af2170a961e71ce67e756b6d12f81150c
Azam Husain <azamhusain9@gmail.com>
2026-08-12 11:29:02 +0800

    build: target production API for login

---
+++ build/assets/recordsTableUiShared-0q_cFwqz.js
@@ -0,0 +1,1 @@
+import{a6 as e}from"./DefaultLayout-D9vgnnAG.js";const r="lg",t=e;export{t as c,r};
--- build/assets/recordsTableUiShared-C9RrXTI1.js
+++
@@ -1,1 +0,0 @@
-import{a6 as e}from"./DefaultLayout-CX4VmsKc.js";const r="lg",t=e;export{t as c,r};
