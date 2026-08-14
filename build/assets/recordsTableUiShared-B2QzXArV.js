4c6fe0c0107a4587df4e965e2b19318b327f282c
Azam Husain <azamhusain9@gmail.com>
2026-08-14 14:44:16 +0800

    build: publish legacy quote cost recovery

---
+++ build/assets/recordsTableUiShared-B2QzXArV.js
@@ -0,0 +1,1 @@
+import{a7 as e}from"./DefaultLayout-Cusi2I32.js";const r="lg",t=e;export{t as c,r};
--- build/assets/recordsTableUiShared-DFimIFuT.js
+++
@@ -1,1 +0,0 @@
-import{a7 as e}from"./DefaultLayout-CUSSHP1s.js";const r="lg",t=e;export{t as c,r};
