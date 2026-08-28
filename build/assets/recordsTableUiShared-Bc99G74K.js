378b53050862c2105d6dd94d866ad9c64d7e0eaf
Azam Husain <azamhusain9@gmail.com>
2026-08-28 16:52:59 +0800

    build: refresh salary and claims release assets

---
+++ build/assets/recordsTableUiShared-Bc99G74K.js
@@ -0,0 +1,1 @@
+import{c as e}from"./tableFormatters-AzjyvDhy.js";const r="lg",t=e;export{t as c,r};
--- build/assets/recordsTableUiShared-DqT3uKdJ.js
+++
@@ -1,1 +0,0 @@
-import{a8 as e}from"./DefaultLayout-CyRXjnzB.js";const r="lg",t=e;export{t as c,r};
